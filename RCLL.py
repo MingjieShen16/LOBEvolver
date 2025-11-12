1637347e207bb16e0eac8cba02b90a6414fc0ec0
unknown <smjdyx@163.com>
2025-11-12 00:25:07 +0000

    Add a test.ipynb

--- RCLL.py
+++ RCLL.py
@@ -4,3 +4,12 @@ def piece_wise_constant_values(times,delta0,t_grid):
     idx = np.searchsorted(times, t_grid, side='right')-1
     idx = np.clip(idx, 0, len(times)-1)
     return delta0[idx]
+
+def extract_windows_from_grid(values, window_size,stride):
+    n_windows = (len(values) - window_size) // stride +1
+    windows = np.zeros((n_windows, window_size, values.shape[1]))
+    for i in range(n_windows):
+        windows[i] = values[i*stride:i*stride + window_size]
+        
+    return windows
+
